3da7a453b6e8ee051319959d4abfa8f9615ffb30
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-04-08 16:29:34 -0300

    A  env.php D  envx.php

---
+++ env.php
@@ -0,0 +1,16 @@
+<?php
+//Criado por Anderson Ismael
+//08 de abril de 2019
+
+require __DIR__.'/vendor/autoload.php';
+
+use Dotenv\Dotenv;
+
+$filename=ROOT.'.env';
+if(file_exists($filename)){
+    $dotenv = Dotenv::create(ROOT);
+    $dotenv->load();
+    print $filename;
+}else{
+    die('nano .env'.PHP_EOL);
+}
